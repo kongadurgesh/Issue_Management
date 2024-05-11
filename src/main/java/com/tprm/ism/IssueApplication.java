325d15354bf6b03a63047aedaaf526fd1947f0d1
Durgesh Konga <kongadurgesh20@gmail.com>
2024-05-11 15:05:11 +0530

    updated main class name

--- src/main/java/com/tprm/ism/IssueApplication.java
+++ src/main/java/com/tprm/ism/IssueApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class IsmApplication {
+public class IssueApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(IsmApplication.class, args);
+		SpringApplication.run(IssueApplication.class, args);
 	}
 
 }
